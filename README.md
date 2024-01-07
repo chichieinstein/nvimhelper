fbc3eda00eb44b73ffb21f380bbc8ea82822e4c0
chichieinstein <chichieinstein@gmail.com>
2024-01-07 09:51:05 -0800

    C/C++ setup file added

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # mynvimplugins
-This repo contains lua files that help setup Neovim for dveelopment in Rust and C++. Features include code autoformatting, code completion, git integration and error highlighting.
+This repo contains lua files that help setup Neovim for development in Rust and C++. Features include code autoformatting, code completion, git integration and error highlighting.
+
+To use this, ensure that
+
+1. Nvim
+2. Cmake
+3. Cargo
+
+are installed. The setup of other required nvim dependencies are taken care of internally.
+
+There is also a bash script, named setup\_project.sh. It takes one commandline argument which is the name of the new C/C++/CUDA project. It does the following actions:
+
+1. Create include and source directories.
+2. Run cmake to create the compile\_commands.json file required by clangd, the C/C++ language server to start tracking the project. 
+
+Run this bash script before starting a new C/C++ project. Once done, fire nvim to start editing source files. Clangd would have everything it needs to start helping you in your development. One final thing to keep in mind is that as new source files are added/removed, the CmakeLists.txt file needs to be updated, and the cmake command needs to be run again.
 
 More plugins would be added in the future.
---
+++ setup_project.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+
+project_name=$1
+cmake_file="CMakeLists.txt"
+
+mkdir -p "$project_name"
+
+cd "$project_name"
+
+mkdir -p "src" && mkdir -p "include"
+
+touch "$cmake_file"
+echo "cmake_minimum_required(VERSION 3.10)" >> "$cmake_file"
+echo "project($project_name LANGUAGES CXX)" >> "$cmake_file"
+echo "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)" >> "$cmake_file"
+echo "set(CMAKE_CXX_STANDARD 17)" >> "$cmake_file"
+echo "add_executable($project_name)" >> "$cmake_file"
+echo "target_sources($project_name PRIVATE ./src/$project_name.cpp ./src/main.cpp)" >> "$cmake_file"
+
+cd "include"
+
+touch "$project_name.h"
+
+echo "#include <iostream>" >> "$project_name.h"
+echo "void display(int);" >> "$project_name.h"
+
+cd "../src"
+touch "$project_name.cpp"
+
+echo "#include \"../include/$project_name.h\"" >> "$project_name.cpp"
+echo "#include <iostream>" >> "$project_name.cpp"
+echo "using std::cout;" >> "$project_name.cpp"
+echo "using std::endl;" >> "$project_name.cpp"
+
+echo "void display(int x)" >> "$project_name.cpp"
+echo "{" >> "$project_name.cpp"
+echo "	cout << x << endl;" >> "$project_name.cpp"
+echo "}" >> "$project_name.cpp"
+
+touch "main.cpp"
+echo "#include \"../include/$project_name.h\"" >> "main.cpp"
+echo "int main()" >> "main.cpp"
+echo "{" >> "main.cpp"
+echo "	int z = 5;" >> "main.cpp"
+echo "	display(z);" >> "main.cpp"
+echo "}" >> "main.cpp"
+
+cd ".."
+cmake -S . -B build
+ln -s build/compile_commands.json .
+
