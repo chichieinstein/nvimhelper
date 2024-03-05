b2739402064c90a5b3a3ada3fc0aeff59e2a0575
chichieinstein <chichieinstein@gmail.com>
2024-03-05 15:35:36 -0800

    Fixed typos

--- ndev_config/nvim/lua/plugins/init.lua
+++ ndev_config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 local merge_tb = vim.tbl_deep_extend
 
 local lsps = require "plugins.lsp"
-local formatters = require "plugins.formatters"
+
 local telescope = require "plugins.telescope"
 local parser = require "plugins.parser"
 local directtree = require "plugins.tree"
@@ -9,6 +9,6 @@ local colorscheme = require "plugins.tokyonight"
 local lines = require "plugins.statusline"
 local preview = require "plugins.preview"
 local tex = require "plugins.tex"
-local config = merge_tb("force", lsps, formatters, telescope, parser, directtree, lines, preview, colorscheme, tex)
+local config = merge_tb("force", lsps,telescope, parser, directtree, lines, preview, colorscheme, tex)
 
 return config
